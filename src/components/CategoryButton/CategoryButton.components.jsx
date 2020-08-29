c2b76880ed368dc78b76e4858bcbc6faa0e4c9e3
Sarayu Gautam <sarayugautam1@gmail.com>
2020-08-29 20:18:55 +0545

    .

--- src/components/CategoryButton/CategoryButton.components.jsx
+++ src/components/CategoryButton/CategoryButton.components.jsx
@@ -1,20 +1,24 @@
-import React, { Fragment } from 'react'
-import './CategoryButton.styles.scss'
+import React, { Fragment } from "react";
+import "./CategoryButton.styles.scss";
 export default function CategoryButton({ label, color, isSelected, onClick }) {
   return (
-    <button
-      onClick={onClick}
-      className='category-button'
-      style={{
-        backgroundColor: color,
-        boxShadow: isSelected && `0 4px 8px 1px ${color}88`,
-      }}>
-      {label && (
-        <Fragment>
-          <span className='capital'>{label[0]}</span>
-          <span className='label'>{label}</span>
-        </Fragment>
+    <div className="button-wrapper">
+      <button
+        onClick={onClick}
+        className="category-button"
+        style={{
+          backgroundColor: color,
+        }}>
+        {label && (
+          <Fragment>
+            <span className="capital">{label[0]}</span>
+            <span className="label">{label}</span>
+          </Fragment>
+        )}
+      </button>
+      {isSelected && (
+        <div className="selected-line" style={{ backgroundColor: color }}></div>
       )}
-    </button>
-  )
+    </div>
+  );
 }
